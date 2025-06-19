1dac7a69f645f499e15522070c3f779a3bbaf607
kron79 <ciaran.brown79@gmail.com>
2025-06-19 16:53:05 +0100

    refactor: convert Storybook config to TypeScript

---
+++ apps/workshop/.storybook/main.ts
@@ -0,0 +1,35 @@
+import { join, dirname } from 'path';
+import { type StorybookConfig } from '@storybook/react-vite';
+
+/**
+ * Resolve absolute path of a package.
+ * This helps when using Yarn PnP or monorepos.
+ */
+function getAbsolutePath(value: string): string {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
+
+const config: StorybookConfig = {
+  stories: [
+    {
+      directory: '../../../packages/ui/src/',
+      titlePrefix: 'UI',
+      files: '*.stories.@(ts|tsx|js|jsx)',
+    },
+  ],
+  addons: [
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@storybook/addon-interactions'),
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: true,
+  },
+};
+
+export default config;
---
+++ apps/workshop/.storybook/preview.ts
@@ -0,0 +1,23 @@
+import 'ui/style';
+import type { Preview } from '@storybook/react';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    backgrounds: {
+      default: 'light',
+      values: [
+        { name: 'light', value: '#ffffff' },
+        { name: 'dark', value: '#0f0f0f' },
+      ],
+    },
+  },
+};
+
+export default preview;
